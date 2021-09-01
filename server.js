3b6b06dd40c6b01a86e063081dede4d55bec534b
Terrel <TerrelRJones@gmail.com>
2021-08-31 19:50:43 -0700

    reseting client

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db = require("./db");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4001;
 
 app.use(cors());
 app.use(json());
