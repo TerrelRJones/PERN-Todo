6e0461e5afe133ee9c65f5ffe65cced614d6c766
Terrel <TerrelRJones@gmail.com>
2021-08-31 19:20:39 -0700

    Moved process.env.NODE_ENV test to before all API calls

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const { Pool } = require("pg");
-require("dotenv").config();
+// require("dotenv").config();
 
 const devConfig = `postgressql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
 
