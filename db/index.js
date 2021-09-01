9dbc00e9a41b1003afafe0c132a0ee138f292e6c
Terrel <TerrelRJones@gmail.com>
2021-08-31 18:34:42 -0700

    reworked files to comply with heroku 8/31/21

--- db/index.js
+++ db/index.js
@@ -1,20 +1,24 @@
 const { Pool } = require("pg");
 
-const devConfig = {
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  password: process.env.PGPASSWORD,
-  databse: process.env.PGDATABASE,
-  port: process.env.PGPORT,
-};
+// const devConfig = {
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   password: process.env.PGPASSWORD,
+//   databse: process.env.PGDATABASE,
+//   port: process.env.PGPORT,
+// };
 
-const proConfig = {
+// const proConfig = {
+//   connectionString: process.env.DATABASE_URL,
+// };
+
+const pool = new Pool({
+  // process.env.NODE_ENV === "production" ? proConfig : devConfig,
   connectionString: process.env.DATABASE_URL,
-};
-
-const pool = new Pool(
-  process.env.NODE_ENV === "production" ? proConfig : devConfig
-);
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
